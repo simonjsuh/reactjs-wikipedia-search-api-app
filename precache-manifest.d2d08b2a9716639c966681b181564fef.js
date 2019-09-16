268d7a285ad81509e3fecb66863941cf1a55b6b3
simonjsuh <simon.j.suh@gmail.com>
2019-09-15 20:57:38 -0700

    Updates

---
+++ precache-manifest.d2d08b2a9716639c966681b181564fef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5976ed6bd873490e7901c62caf53af7f",
+    "url": "/reactjs-wikipedia-search-api-app/index.html"
+  },
+  {
+    "revision": "b5c57faa6d86974eea12",
+    "url": "/reactjs-wikipedia-search-api-app/static/css/main.cdf0f34f.chunk.css"
+  },
+  {
+    "revision": "d7296d093d663b3d93cc",
+    "url": "/reactjs-wikipedia-search-api-app/static/js/2.d25b1718.chunk.js"
+  },
+  {
+    "revision": "b5c57faa6d86974eea12",
+    "url": "/reactjs-wikipedia-search-api-app/static/js/main.9555ed95.chunk.js"
+  },
+  {
+    "revision": "bc0cf313c8fbfb6a77a7",
+    "url": "/reactjs-wikipedia-search-api-app/static/js/runtime~main.42bce36e.js"
+  }
+]);
